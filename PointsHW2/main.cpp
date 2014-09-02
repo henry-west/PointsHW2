c94fda394ae00f940a8d78fd47599fdbc6698f91
Henry West <henry@henrys-mba.ddns.drake.edu>
2014-09-02 12:06:14 -0500

    Initial Commit

---
+++ PointsHW2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PointsHW2
+//
+//  Created by Henry West on 9/2/14.
+//  Copyright (c) 2014 Henry West. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
